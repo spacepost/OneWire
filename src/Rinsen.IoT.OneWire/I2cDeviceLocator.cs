b471350a922e1dda0e5a3361d6bd9e382de2a5ec
Sean Bibbey <sean.bibbey@gmail.com>
2019-01-09 18:11:34 -0500

    Add lightning driver support

--- src/Rinsen.IoT.OneWire/I2cDeviceLocator.cs
+++ src/Rinsen.IoT.OneWire/I2cDeviceLocator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.IoT.Lightning.Providers;
+using System;
 using Windows.Devices.Enumeration;
 using Windows.Devices.I2c;
 
@@ -11,6 +12,24 @@ namespace Rinsen.IoT.OneWire
 
         public I2cDevice GetI2cDevice(byte address)
         {
+            // Check if Lightning is enabled and set the Lightning provider as the default provider
+            if (Microsoft.IoT.Lightning.Providers.LightningProvider.IsLightningEnabled)
+            {
+                Windows.Devices.LowLevelDevicesController.DefaultProvider =
+                    Microsoft.IoT.Lightning.Providers.LightningProvider.GetAggregateProvider();
+                var i2CProvider = LightningI2cProvider.GetI2cProvider();
+                var i2CControllers = I2cController.GetControllersAsync(i2CProvider).AsTask().Result;
+                var i2CController = i2CControllers[0];
+                var i2CDevice = i2CController.GetDevice(new I2cConnectionSettings(address));
+                if (i2CDevice != null)
+                {
+                    i2CDevice.ConnectionSettings.BusSpeed = I2cBusSpeed.FastMode;
+                    return i2CDevice;
+                }
+
+                throw new InvalidOperationException("No I2C controllers were found on the system");
+            }
+
             string aqs = I2cDevice.GetDeviceSelector(I2C_CONTROLLER_NAME);                     /* Get a selector string that will return all I2C controllers on the system */
             var deviceInformation = DeviceInformation.FindAllAsync(aqs).AsTask().Result;            /* Find the I2C bus controller device with our selector string           */
             if (deviceInformation.Count == 0)
